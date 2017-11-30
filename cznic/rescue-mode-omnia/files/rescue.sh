46ba5e56dc3d347301cae500222275fe910c7db6
Michal Hrušecký <michal.hrusecky@nic.cz>
2017-11-30 13:35:58 +0100

    fixup! fixup! Flashing rescue

--- cznic/rescue-mode-omnia/files/rescue.sh
+++ cznic/rescue-mode-omnia/files/rescue.sh
@@ -51,127 +51,127 @@ BIN_MDEV="`which mdev`"
 [ -z "$BIN_MDEV" ] || BIN_MDEV="$BIN_MDEV -s"
 
 d() {
-	if [ $DEBUG -gt 0 ]; then
-		echo "$@"
-	fi
+    if [ $DEBUG -gt 0 ]; then
+        echo "$@"
+    fi
 }
 
 e() {
-	echo "Error: $@" >&2
+    echo "Error: $@" >&2
 }
 
 mount_fs() {
-	d "mount_fs $1 $2"
-	if ! [ -d $2 ]; then
-		$BIN_MKDIR -p "$2"
-	fi
+    d "mount_fs $1 $2"
+    if ! [ -d $2 ]; then
+        $BIN_MKDIR -p "$2"
+    fi
 
-	if $BIN_MOUNT | $BIN_GREP -E "on $2 ">/dev/null; then
-		e "Mountpoint $2 already used. Exit."
-		exit 21
-	fi
+    if $BIN_MOUNT | $BIN_GREP -E "on $2 ">/dev/null; then
+        e "Mountpoint $2 already used. Exit."
+        exit 21
+    fi
 
-	$BIN_MOUNT $1 $2
-	if [ $? -ne 0 ]; then
-		e "Mount $1 on $2 failed."
-		return 22
-	fi
+    $BIN_MOUNT $1 $2
+    if [ $? -ne 0 ]; then
+        e "Mount $1 on $2 failed."
+        return 22
+    fi
 }
 
 umount_fs() {
-	d "umount_fs $1"
-	$BIN_UMOUNT $1
-	if $BIN_MOUNT | $BIN_GREP -E "on $1 ">/dev/null; then
-		$BIN_UMOUNT -fl $1
-	fi
-	if $BIN_MOUNT | $BIN_GREP -E "on $1 ">/dev/null; then
-		$BIN_MOUNT -o remount,ro $1
-	fi
-	$BIN_SYNC
+    d "umount_fs $1"
+    $BIN_UMOUNT $1
+    if $BIN_MOUNT | $BIN_GREP -E "on $1 ">/dev/null; then
+        $BIN_UMOUNT -fl $1
+    fi
+    if $BIN_MOUNT | $BIN_GREP -E "on $1 ">/dev/null; then
+        $BIN_MOUNT -o remount,ro $1
+    fi
+    $BIN_SYNC
 }
 
 do_warning() {
-	umount_fs $SRCFS_MOUNTPOINT
-	while true; do
-		rainbow all enable blue
-		sleep 1
-		rainbow all disable
-		sleep 1
-	done
+    umount_fs $SRCFS_MOUNTPOINT
+    while true; do
+        rainbow all enable blue
+        sleep 1
+        rainbow all disable
+        sleep 1
+    done
 }
 
 do_panic() {
-	umount_fs $SRCFS_MOUNTPOINT
-	while true; do
-		rainbow all enable red
-		sleep 1
-		rainbow all disable
-		sleep 1
-	done
+    umount_fs $SRCFS_MOUNTPOINT
+    while true; do
+        rainbow all enable red
+        sleep 1
+        rainbow all disable
+        sleep 1
+    done
 }
 
 reflash () {
-	IMG=""
-	# find image (give it 10 tries with 1 sec delay)
-	for i in `seq 1 10`; do
-		$BIN_MDEV
-		for dev in `$BIN_AWK '$4 ~ /sd[a-z]+[0-9]?/ { print $4 }' /proc/partitions`; do
-			d "Testing FS on device $dev"
-			[ -b "/dev/$dev" ] || continue
-			mount_fs "/dev/$dev" $SRCFS_MOUNTPOINT
-			if [ $? -ne 0 ]; then
-				continue
-			fi
-			d "Searching for ${SRCFS_MOUNTPOINT}/${MEDKIT_FILENAME} on $dev"
-			IMG="$(ls -1d ${SRCFS_MOUNTPOINT}/to_flash | sort | tail -n 1)"
-			if [ -n "${IMG}" ] && [ -d "${SRCFS_MOUNTPOINT}/to_flash" ]; then
-				d "Found medkit file $IMG on device $dev"
-				break
-			else
-				IMG=""
-				d "Medkit file not found on device $dev"
-				umount_fs $SRCFS_MOUNTPOINT
-			fi
-		done
-		[ -z "${IMG}" ] || break
-		sleep 1
-	done
+    IMG=""
+    # find image (give it 10 tries with 1 sec delay)
+    for i in `seq 1 10`; do
+        $BIN_MDEV
+        for dev in `$BIN_AWK '$4 ~ /sd[a-z]+[0-9]?/ { print $4 }' /proc/partitions`; do
+            d "Testing FS on device $dev"
+            [ -b "/dev/$dev" ] || continue
+            mount_fs "/dev/$dev" $SRCFS_MOUNTPOINT
+            if [ $? -ne 0 ]; then
+                continue
+            fi
+            d "Searching for ${SRCFS_MOUNTPOINT}/${MEDKIT_FILENAME} on $dev"
+            IMG="$(ls -1d ${SRCFS_MOUNTPOINT}/to_flash | sort | tail -n 1)"
+            if [ -n "${IMG}" ] && [ -d "${SRCFS_MOUNTPOINT}/to_flash" ]; then
+                d "Found medkit file $IMG on device $dev"
+                break
+            else
+                IMG=""
+                d "Medkit file not found on device $dev"
+                umount_fs $SRCFS_MOUNTPOINT
+            fi
+        done
+        [ -z "${IMG}" ] || break
+        sleep 1
+    done
 
-	if [ -n "${IMG}" ]; then
-    if [ -r "${IMG}"/rootfs.img ]; then
-		  if [ -r "${IMG}"/rootfs.img.md5 ]; then
-				cd "${IMG}"
-          md5sum -c rootfs.img.md5 || do_warning
-				cd
-      fi
-      dd if="${IMG}"/rootfs.img of=$DEV bs=1M || do_warning
-		fi
+    if [ -n "${IMG}" ]; then
+        if [ -r "${IMG}"/rootfs.img ]; then
+            if [ -r "${IMG}"/rootfs.img.md5 ]; then
+                cd "${IMG}"
+                md5sum -c rootfs.img.md5 || do_warning
+                cd
+            fi
+            dd if="${IMG}"/rootfs.img of=$DEV bs=1M || do_warning
+        fi
     if [ -r "${IMG}"/uboot.img ]; then
-		  if [ -r "${IMG}"/uboot.img.md5 ]; then
-				cd "${IMG}"
-          md5sum -c uboot.img.md5 || do_warning
-				cd
-      fi
-			mtd erase /dev/mtd0
-			mtd write uboot.img /dev/mtd0 || do_warning
-		fi
+          if [ -r "${IMG}"/uboot.img.md5 ]; then
+              cd "${IMG}"
+              md5sum -c uboot.img.md5 || do_warning
+              cd
+          fi
+          mtd erase /dev/mtd0
+          mtd write "${IMG}"/uboot.img /dev/mtd0 || do_warning
+    fi
     if [ -r "${IMG}"/rescue.img ]; then
-		  if [ -r "${IMG}"/rescue.img.md5 ]; then
-				cd "${IMG}"
-          md5sum -c rescue.img.md5 || do_warning
-				cd
-      fi
-			mtd erase /dev/mtd1
-			mtd write rescue.img /dev/mtd1 || do_warning
-		fi
-		umount_fs $SRCFS_MOUNTPOINT
+          if [ -r "${IMG}"/rescue.img.md5 ]; then
+              cd "${IMG}"
+              md5sum -c rescue.img.md5 || do_warning
+              cd
+           fi
+           mtd erase /dev/mtd1
+           mtd write "${IMG}"/rescue.img /dev/mtd1 || do_warning
+        fi
+        umount_fs $SRCFS_MOUNTPOINT
     sync
-	else
-		d "No medkit image found. Please reboot."
-		do_warning
-	fi
+    else
+        d "No medkit image found. Please reboot."
+        do_warning
+    fi
 
-	d "Reflash succeeded."
+    d "Reflash succeeded."
 }
 
 
@@ -182,7 +182,7 @@ reflash
 rainbow all enable ffff00
 
 while true; do
-	sleep 10
+    sleep 10
 done
 
 exit 0
